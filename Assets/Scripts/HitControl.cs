091281e51387d151811ea4e02ecb767def4eae3c
Zurab Gezalyan <mcdoss@mail.ru>
2024-07-15 14:13:03 +0300

    Task complete

---
+++ Assets/Scripts/HitControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitControl : MonoBehaviour
+{
+
+    public float forceAmount;    
+    
+    public void Hit(GameObject ball)
+    {
+        Vector3 destinationCenter = ball.GetComponent<SphereCollider>().bounds.center;
+        Vector3 objectCenter = GetComponent<Collider>().bounds.center;
+        Debug.Log(destinationCenter);
+        Debug.Log(objectCenter);
+        Vector3 dirrection = objectCenter - destinationCenter;
+        Debug.Log(dirrection);
+        GetComponent<Rigidbody>().AddForce(dirrection * forceAmount, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<SphereCollider>() != null)
+            Hit(collision.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/PushControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class PushControl : MonoBehaviour
+{
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+    public float speed;
+    public float force;
+
+    private Vector3 _destination;
+    
+    void Start()
+    {
+        _destination = endPoint.transform.position;
+    }
+
+    void Update()
+    {
+        if (transform.position != _destination)
+            transform.position = Vector3.MoveTowards(transform.position, _destination, Time.deltaTime * speed);
+        else
+        {
+            if (_destination == startPoint.transform.position)
+                _destination = endPoint.transform.position;
+            else
+                _destination = startPoint.transform.position;            
+        }
+    }
+
+    public void Push(GameObject ball)
+    {
+        Vector3 destinationCenter = ball.GetComponent<SphereCollider>().bounds.center;
+        Vector3 objectCenter = GetComponent<CapsuleCollider>().bounds.center;        
+        Vector3 dirrection =  destinationCenter - objectCenter;
+        ball.GetComponent<Rigidbody>().AddForce(dirrection * force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<SphereCollider>() != null)
+            Push(collision.gameObject);
+    }
+}
